e76c9200af284a1b4fcb36f9982188b89925844c
Janet Castillo <janetcastillo56@gmail.com>
2023-11-14 15:41:20 -0700

    images-changed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const app = express();
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
+const notFound = require("./errors/notFound")
+const errorHandler= require("./errors/errorHandler")
+
 
 app.use(cors());
 app.use(express.json());
@@ -14,15 +17,9 @@ app.use("/reviews", reviewsRouter);
 app.use("/theaters", theatersRouter);
 
 // Not found handler
-app.use((req, res, next) => {
-  next({ status: 404, message: `Not found: ${req.originalUrl}` });
-});
+app.use(notFound);
 
 // Error handler
-app.use((error, req, res, next) => {
-  console.error(error);
-  const { status = 500, message = "Something went wrong!" } = error;
-  res.status(status).json({ error: message });
-});
+app.use(errorHandler);
 
 module.exports = app;
